c9c70143ae5d585712ee9a65e015eb13f0cf5597
Percy Leso <percy@icollective.agency>
2022-07-21 15:09:14 +0200

    commit recent ui changes

--- PhoneBookApp/PhoneBook/UI-Angular/AngularPhonebook/src/app/app.module.ts
+++ PhoneBookApp/PhoneBook/UI-Angular/AngularPhonebook/src/app/app.module.ts
@@ -9,11 +9,10 @@ import { PhonebookComponent } from './phonebook/phonebook.component';
 import { AddEditEntryComponent } from './phonebook/add-edit-entry/add-edit-entry.component';
 import { ViewPhonebookComponent } from './phonebook/view-phonebook/view-phonebook.component';
 import { PhonebookApiService } from './Shared/phonebook-api.service';
-import { AppRoutingModule } from './app-routing.module'
 import { LoginComponent } from './Auth/login/login.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { AppRoutingModule } from './app-routing.module';
 import { environment } from 'src/environments/environment.dev';
-import { TokenInterceptor } from './Auth/token.interceptor';
 import { JwtTokenInterceptorService } from './Auth/jwt-token-interceptor.service';
 
 export function getToken() {
